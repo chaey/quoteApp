3b5b47f4a9fa54e362686d837c2275481ae55dc3
chaey <foxrhee@gmail.com>
2022-06-23 14:09:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function randomQuote(){
 speechBtn.addEventListener("click", ()=>{
     if(!quoteBtn.classList.contains("loading")){
         let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        utterance.lang='en-US';
         synth.speak(utterance);
         setInterval(()=>{
             synth.speaking? speechBtn.classList.add("active"):speechBtn.classList.remove("active");
